3c83e25542916ae3b7a883d7b99d9c29aa927542
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-10-17 09:32:55 -0300

    Creacion y utilizacion de Dto

--- Modelos/Villa.cs
+++ Modelos/Villa.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Nombre { get; set; }
 
-        //public DateTime FechaCreacion { get; set; }
+        public DateTime FechaCreacion { get; set; }
     }
 }
