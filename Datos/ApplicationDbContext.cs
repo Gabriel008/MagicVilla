62f77e9d590fe2264c61b27dc642c137925a67c6
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-10-18 19:53:10 -0300

    _Cadena de conexion DbSetIdentity 1.53.58

---
+++ Datos/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_API.Datos
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public DbSet<Villa> Villas { get; set; }
+
+    }
+}
