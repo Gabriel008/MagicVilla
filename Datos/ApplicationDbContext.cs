d80f0943f2c5e2c5bffb9ae9d341b5c7d236888b
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-10-18 21:36:54 -0300

    Usar la cadena de conexion 1.58.58 2.03.08

--- Datos/ApplicationDbContext.cs
+++ Datos/ApplicationDbContext.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MagicVilla_API.Modelos;
+using Microsoft.EntityFrameworkCore;
 
 namespace MagicVilla_API.Datos
 {
     public class ApplicationDbContext :DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+            
+        }
         public DbSet<Villa> Villas { get; set; }
 
     }
