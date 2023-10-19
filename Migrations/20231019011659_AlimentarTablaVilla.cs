10068b7d753f776c88a9336882eba14e797c303b
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-10-18 22:22:07 -0300

    Alimentar Tabla Villa 2.08.43 2.14.37

---
+++ Migrations/20231019011659_AlimentarTablaVilla.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MagicVilla_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlimentarTablaVilla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Amenidad", "Detalle", "FechaActualizacion", "FechaCreacion", "ImagenUrl", "MetrosCuadrados", "Nombre", "Ocupantes", "Tarifa" },
+                values: new object[,]
+                {
+                    { 1, "", "Detalle..", new DateTime(2023, 10, 18, 22, 16, 59, 74, DateTimeKind.Local).AddTicks(6553), new DateTime(2023, 10, 18, 22, 16, 59, 74, DateTimeKind.Local).AddTicks(6511), "", 50, "Villa Real", 5, 200.0 },
+                    { 2, "", "Detalle..", new DateTime(2023, 10, 18, 22, 16, 59, 74, DateTimeKind.Local).AddTicks(6557), new DateTime(2023, 10, 18, 22, 16, 59, 74, DateTimeKind.Local).AddTicks(6556), "", 50, "Premium Vista a la Pisina", 4, 500.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
